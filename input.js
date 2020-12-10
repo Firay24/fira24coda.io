a7ee4f80cc49768d899a93d78a0be9e856a8debd
firay24 <72513085+Firay24@users.noreply.github.com>
2020-12-10 23:58:12 +0700

    Add files via upload

---
+++ input.js
@@ -0,0 +1,6 @@
+alert("Selamat Datang")
+function submitdata() {
+    var username = document.getElementsByClassName("user").value;
+    var password = document.getElementsByClassName("pass").value;
+    alert(username + password)
+}
